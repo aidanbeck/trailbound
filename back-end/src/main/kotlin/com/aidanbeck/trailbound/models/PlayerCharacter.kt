0f005d1f8207d62b5d3ded7f0c3894104dbb7a05
aidanbeck <aidanmbeck@outlook.com>
2025-12-26 18:13:59 -0600

    adjust PlayerCharacter class to fit blueprints

--- back-end/src/main/kotlin/com/aidanbeck/trailbound/models/PlayerCharacter.kt
+++ back-end/src/main/kotlin/com/aidanbeck/trailbound/models/PlayerCharacter.kt
@@ -1,15 +1,16 @@
 package com.aidanbeck.trailbound.models
 
 class PlayerCharacter(
-    val x: Int = 0,
-    val y: Int = 0,
+    val name: String = "Hiker",
+    val pinNumber: Int? = null, // Null = no pin, character isn't locked.
     val symbol: Char = '&',
-    val gamePin: Int = 0,
-    val hatchets: Int = 4, // # of times the player can break tiles
-    val walls: Int = 0     // # of times the player can place tiles
+    var x: Int = 0,
+    var y: Int = 0,
+    var energy: Int = 4
     ) {
-    fun move(world: World, xOffset: Int, yOffset: Int) {}
-    fun placeTile(world: World, xOffset: Int, yOffset: Int, char: Char) {}
-    fun getNearbyTiles(world: World, squareRadius: Int): String = "_"
-    fun printNearbyTiles(world: World, squareRadius: Int) {}
+    fun input(direction: String, world: World) {}
+    fun move(xOffset: Int, yOffset: Int, world: World) {}
+    fun dismantle(xOffset: Int, yOffset: Int, world: World) {}
+    fun take(xOffset: Int, yOffset: Int, world: World) {}
+    fun getNearbyTiles(world: World, squareRadius: Int): String { return "....." }
 }
